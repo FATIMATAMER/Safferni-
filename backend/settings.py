6cbb91b317da5de342d2c2a38b829ebdcc17a212
ali123ezzo <ali123ezzo@gmail.com>
2025-05-02 11:11:46 +0300

    page not found ...

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o@3q7@3sr_nfk^z7&24y+tj#rgvdf^%5&+rnd*ngki0^vz7r*#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
