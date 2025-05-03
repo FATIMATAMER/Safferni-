4a34b8fb11cf99b0744a39eb24fa9a02ab125f2d
ali123ezzo <ali123ezzo@gmail.com>
2025-05-03 12:55:06 +0300

    User Authentication Modifications

--- companyManagement/api.py
+++ companyManagement/api.py
@@ -1,10 +1,15 @@
 from rest_framework import viewsets
 from .models import Company
 from .serializers import CompanySerializer
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+)
+
 
 class CompanyViewSet(viewsets.ModelViewSet):
+
     queryset=Company.objects.all()
     serializer_class=CompanySerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
 
-
-   
--- trips/api.py
+++ trips/api.py
@@ -1,7 +1,30 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from .models import Trip
 from .serializers import TripSerializer
+from .permissions import IsManager, IsEployee
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+    AllowAny
+)
+
 
 class TripViewSet(viewsets.ModelViewSet):
+    
     queryset=Trip.objects.all()
     serializer_class=TripSerializer
+    permission_classes = [IsManager, IsAdminUser, IsAuthenticated]
+
+
+# class TripView(generics.RetrieveUpdateDestroyAPIView):
+
+#     queryset = Trip.objects.all()
+#     serializer_class = TripSerializer
+    
+#     def get_permissions(self):
+#         if self.request.method == 'GET':
+#             permission_classes = [AllowAny]
+#         else:
+#             permission_classes = [IsAuthenticated, IsManager]
+#         return [permission() for permission in permission_classes]
+    
