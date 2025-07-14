43e8eb49a918ce4c23bd989ee180647bd0746582
Mayar <mayarah396@gmail.com>
2025-07-14 14:36:32 +0300

    Change token lifetime

--- backend/settings.py
+++ backend/settings.py
@@ -173,8 +173,8 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
 
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': True,
